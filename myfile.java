ac7ff7295a1df0258852a6f50bfd6ae474189027
shubham <shunhamtande2002@gmail.com>
2025-01-15 10:11:37 +0000

    my commit1

--- myfile.java
+++ myfile.java
@@ -1,12 +1,7 @@
-class myfile {
-
-  public static void main(String[] args) {
-    
-    int first = 10;
-    int second = 20;
-
-    // add two numbers
-    int sum = first + second;
-    System.out.println(first + " + " + second + " = "  + sum);
-  }
+class myfile
+{
+    public static void main(String[] args)
+    {
+        System.out.println("My First Java Program.");
+    }
 }
